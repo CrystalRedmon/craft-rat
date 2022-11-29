74b6405e8164df429f3293696d765d6d7d9759a1
Crystal Redmon <cato2848@gmail.com>
2022-11-28 21:47:08 -0600

    complete styling for about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -12,88 +12,86 @@ function AboutPage() {
 
     // container for the entire page
     <Grid container>
-      <Grid item xs={5}></Grid>
 
+      <Grid item xs={5}></Grid>
       <Grid item xs={7}>
         <h1>About CraftRat</h1>
       </Grid>
 
+      <Grid item xs={2}></Grid>
 
-      <Grid item xs={2.5}></Grid>
-      <Grid container item xs={5}>
-        <Card sx={{ minWidth: 800, minHeight: 500 }}>
+      <Grid item xs={3.5}>
+        <Card sx={{ minWidth: 350, minHeight: 350 }}>
           <CardContent>
+            <Box sx={{ marginBottom: '20px' }}>
+              <Typography>
+                Technologies Used
+                <ul>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Javascript</li>
+                  <li>Express</li>
+                  <li>PostgreSQL</li>
+                  <li>MUI</li>
+                </ul>
+              </Typography>
+            </Box>
 
-            <Stack direction={'rows'}>
-              <Box sx={{ width: '50%', marginBottom: '20px' }}>
+            <Box>
+              <Box sx={{ height: '50%', marginBottom: '20px' }}>
                 <Typography>
-                  Technologies Used
-                  <ul>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>Javascript</li>
-                    <li>Express</li>
-                    <li>PostgreSQL</li>
-                    <li>MUI</li>
-                  </ul>
+                  Future versions...
+                </Typography><br />
+                <Typography>
+                  Future versions will integrate a calendar to alert multicrafters of upcoming events such as birthdays.
                 </Typography>
               </Box>
 
-              <Box sx={{ width: '10%' }}></Box>
-
-              <Box alignItems={'center'}>
+              <Box sx={{ height: '50%' }}>
                 <Typography>
-                  Let's Connect!
-                </Typography>
-                <img src={image1} alt='linkedIn QR code' style={{ width: 200, height: 200 }}></img>
-              </Box>
-            </Stack>
-
-            <Stack direction={'rows'}>
-
-              <Box sx={{ width: '50%' }}>
-
-                <Box sx={{ height: '50%', marginBottom: '20px' }}>
-                  <Typography>
-                    Future versions
-                  </Typography><br/>
-                  <Typography>
-                    Future versions will integrate a calendar to alert multicrafters of upcoming events such as birthdays.
-                  </Typography>
-                </Box>
-
-                <Box sx={{ height: '50%' }}>
-                  <Typography>
-                    Thank you...
-                  </Typography><br />
-                  <Typography>
-                    Thank you to Prime, the Ramirez cohort, and, our instructor, Edan.
-                    Special thanks to my family for their support.
-                  </Typography>
-                </Box>
-
-
-              </Box>
-              <Box sx={{width: '10%'}}></Box>
-              <Box mt={5}>
+                  Thank you...
+                </Typography><br />
                 <Typography>
-                  Check out my <a href='https://github.com/CrystalRedmon'>GitHub</a>
+                  Thank you to Prime, the Ramirez cohort, and, our instructor, Edan.
+                  Special thanks to my family for their support.
                 </Typography>
               </Box>
+            </Box>
+          </CardContent>
+        </Card>
 
 
-            </Stack>
+      </Grid>
+
+      <Grid item xs={1}></Grid>
 
 
+      <Grid item xs={3.5}>
+        <Card sx={{ minWidth: 350, minHeight: 350 }}>
+          <CardContent>
+            <Box>
+              <Typography sx={{textAlign: 'center'}}>
+                Let's Connect!
+              </Typography>          
+            </Box>
 
+            <Box ml={5}>
+              <Typography>
+                LinkedIn:
+              </Typography>
+              <img src={image1} alt='linkedIn QR code' style={{ width: 300, height: 300 }}></img>
+              <Typography mt={5} mb={5}>
+                Check out my <a href='https://github.com/CrystalRedmon'>GitHub</a>
+              </Typography>
+
+            </Box>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={4.5}></Grid>
+      <Grid item xs={3}>
 
-
-
-    </Grid>
+      </Grid>
+    </Grid >
   );
 }
 
